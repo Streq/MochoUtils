ab61c409fb8a1b8d235b225d7c003f324b8fa1fb
Santiago Requena <strequena@gmail.com>
2018-06-12 00:34:08 -0300

    branchito con require para bundlear con browserify

--- MochoUtils/mocho.input.js
+++ MochoUtils/mocho.input.js
@@ -1,65 +1,63 @@
 "use strict";
-var Mocho = (function(mod){
-	
-	/**
-	 * EventListenerQueue Factory
-	 * @function
-	 * @param {Element} [element] - the element events will be relative to,
-	 * @param {String[]} [types] - event types that will be listened to.
-	 * @param {function(Event)} [filter] - filter function that filters and/or transforms events,
-	 *        it must return null or undefined if the event is filtered out, the transformed value otherwise.
-	 */
-	function makeEventListenerQueue(element, types, filter){
-		var queue = new Mocho.DBufferQueue();
-		
-        if(types != null){
-            types.forEach(function(type){
-                addListener(queue, element, type, filter);
-            });
-		}
-		return queue;
-	}
+var structs = require("./mocho.structs");
+
+var mod = module.exports
+/**
+ * EventListenerQueue Factory
+ * @function
+ * @param {Element} [element] - the element events will be relative to,
+ * @param {String[]} [types] - event types that will be listened to.
+ * @param {function(Event)} [filter] - filter function that filters and/or transforms events,
+ *        it must return null or undefined if the event is filtered out, the transformed value otherwise.
+ */
+function makeEventListenerQueue(element, types, filter){
+    var queue = new structs.DBufferQueue();
     
-    /**
-	 * Add listener to queue
-	 * @function
-     * @param {Mocho.DBufferQueue} queue - the event queue
-	 * @param {Element} element - the element events will be relative to,
-	 * @param {String} types - event type that will be listened to.
-	 * @param {function(Event)} [filter] - filter function that filters and/or transforms events,
-	 *        it must return null or undefined if the event is filtered out, the transformed value otherwise.
-	 */
-    function addListener(queue, element, type, filter){
-        element.addEventListener(type,function(ev){
-            let transformed = filter? filter(ev) : ev;
-            if(transformed!=null){
-                queue.enqueue(transformed);
-            }
+    if(types != null){
+        types.forEach(function(type){
+            addListener(queue, element, type, filter);
         });
     }
-    
-    var preventDefaultArrowKeys = (event) => {
-        switch(event.keyCode){
-            case 37:
-            case 38:
-            case 39:
-            case 40:
-                event.view.event.preventDefault();
-        }  
-    };
-    
-    function avoidArrowKeyScroll(element){
-        element.addEventListener("keydown", preventDefaultArrowKeys)
-    }
-    
-    function allowArrowKeyScroll(element){
-        element.removeEventListener("keydown", preventDefaultArrowKeys);
-    }
-    
-	mod.makeEventQueue = makeEventListenerQueue;
-    mod.addListener = addListener;
-    mod.avoidArrowKeyScroll = avoidArrowKeyScroll;
-    mod.allowArrowKeyScroll = allowArrowKeyScroll;
-	return mod;
-	
-})(Mocho||{});
+    return queue;
+}
+
+/**
+ * Add listener to queue
+ * @function
+ * @param {DBufferQueue} queue - the event queue
+ * @param {Element} element - the element events will be relative to,
+ * @param {String} types - event type that will be listened to.
+ * @param {function(Event)} [filter] - filter function that filters and/or transforms events,
+ *        it must return null or undefined if the event is filtered out, the transformed value otherwise.
+ */
+function addListener(queue, element, type, filter){
+    element.addEventListener(type,function(ev){
+        let transformed = filter? filter(ev) : ev;
+        if(transformed!=null){
+            queue.enqueue(transformed);
+        }
+    });
+}
+
+var preventDefaultArrowKeys = (event) => {
+    switch(event.keyCode){
+        case 37:
+        case 38:
+        case 39:
+        case 40:
+            event.view.event.preventDefault();
+    }  
+};
+
+function avoidArrowKeyScroll(element){
+    element.addEventListener("keydown", preventDefaultArrowKeys)
+}
+
+function allowArrowKeyScroll(element){
+    element.removeEventListener("keydown", preventDefaultArrowKeys);
+}
+
+mod.makeEventQueue = makeEventListenerQueue;
+mod.addListener = addListener;
+mod.avoidArrowKeyScroll = avoidArrowKeyScroll;
+mod.allowArrowKeyScroll = allowArrowKeyScroll;
--- MochoUtils/mocho.structs.js
+++ MochoUtils/mocho.structs.js
@@ -1,44 +1,41 @@
 "use strict";
-var Mocho = (function(mod){
-	
-	function Queue(){
-		this.array=[];
-	}
-	Queue.prototype.enqueue=function(el){
-		this.array.push(el);
-	}
-	Queue.prototype.dequeue=function(){
-		return this.array.shift();
-	}
-	Queue.prototype.isEmpty=function(){
-		return this.array.length == 0;
-	}
-	Queue.prototype.length = function(){
-		return this.array.length;
-	}
-	
-	function DBufferQueue(){
-		this.first = new Queue();
-		this.second = new Queue();
-		this.inqueue = this.first;
-		this.outqueue = this.second;
-	}
-	DBufferQueue.prototype.enqueue=function(el){
-		this.inqueue.enqueue(el);
-	}
-	DBufferQueue.prototype.dequeue=function(){
-		return this.outqueue.dequeue();
-	}
-	DBufferQueue.prototype.swapBuffer = function(){
-		let aux = this.inqueue;
-		this.inqueue = this.outqueue;
-		this.outqueue = aux;
-	}
-	DBufferQueue.prototype.isEmpty = function(){
-		return this.outqueue.isEmpty();
-	}
-	mod.Queue = Queue;
-	mod.DBufferQueue = DBufferQueue;
-	return mod;
-	
-})(Mocho||{});
+var mod = module.exports;
+
+function Queue(){
+	this.array=[];
+}
+Queue.prototype.enqueue=function(el){
+	this.array.push(el);
+}
+Queue.prototype.dequeue=function(){
+	return this.array.shift();
+}
+Queue.prototype.isEmpty=function(){
+	return this.array.length == 0;
+}
+Queue.prototype.length = function(){
+	return this.array.length;
+}
+
+function DBufferQueue(){
+	this.first = new Queue();
+	this.second = new Queue();
+	this.inqueue = this.first;
+	this.outqueue = this.second;
+}
+DBufferQueue.prototype.enqueue=function(el){
+	this.inqueue.enqueue(el);
+}
+DBufferQueue.prototype.dequeue=function(){
+	return this.outqueue.dequeue();
+}
+DBufferQueue.prototype.swapBuffer = function(){
+	let aux = this.inqueue;
+	this.inqueue = this.outqueue;
+	this.outqueue = aux;
+}
+DBufferQueue.prototype.isEmpty = function(){
+	return this.outqueue.isEmpty();
+}
+mod.Queue = Queue;
+mod.DBufferQueue = DBufferQueue;
